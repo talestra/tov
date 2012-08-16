a8ee11db567abcc1bf7a1f1ef47cc5c212331695
soywiz <soywiz@gmail.com>
2012-08-16 03:57:12 +0200

    - More Work

---
+++ TalesOfVesperiaUtils/TalesOfVesperiaUtils/Imaging/BitmapList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace TalesOfVesperiaUtils.Imaging
+{
+	public class BitmapList
+	{
+		public Bitmap[] Bitmaps;
+
+		public BitmapList(int Count)
+		{
+			this.Bitmaps = new Bitmap[Count];
+		}
+	}
+}
