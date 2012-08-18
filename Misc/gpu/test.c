d8c41ee413a9b427718cffec66940c7a2bd33c38
soywiz <soywiz@gmail.com>
2012-08-18 13:32:20 +0200

    - Small updates

--- Misc/gpu/test.c
+++ Misc/gpu/test.c
@@ -1,6 +1,6 @@
 #include "xgraphics_test.h"
 
-int main(char** argv, int argc) {
+void test_xy() {
 	int n;
 	int width = 16;
 	int height = 15;
@@ -11,5 +11,27 @@ int main(char** argv, int argc) {
 		int y = XGAddress2DTiledY(n, width, texelpitch);
 		printf("n=%d: (x=%d, y=%d)\n", n, x, y);
 	}
+}
+
+void test_offset() {
+	int x, y, n;
+	int width = 16;
+	int height = 15;
+	int depth = 1;
+	int texelpitch = 16;
+	
+	printf("size: %d\n", XGAddress2DTiledExtent(width, height, texelpitch));
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			n = XGAddress2DTiledOffset(x, y, width, texelpitch);
+			printf("(x=%d, y=%d): n=%d\n", x, y, n);
+		}
+	}
+}
+
+int main(char** argv, int argc) {
+	//test_xy();
+	test_offset();
 	return 0;
 }
