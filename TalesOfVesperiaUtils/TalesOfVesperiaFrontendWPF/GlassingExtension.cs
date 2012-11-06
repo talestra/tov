1aed7e8c742efb8a6b9d5a21e64b74e59d4c1e5d
soywiz <soywiz@gmail.com>
2012-11-06 10:18:45 +0100

    - Updates

---
+++ TalesOfVesperiaUtils/TalesOfVesperiaFrontendWPF/GlassingExtension.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace TalesOfVesperiaFrontendWPF
+{
+	public static class GlassingExtension
+	{
+		/// <summary>
+		/// Sets glass background to whole window.
+		/// </summary>
+		/// <remarks>Remember to set your WPF Window Background to "Transparent"!</remarks>
+		/// <param name="win"></param>
+		public static void GlassBackground(this Window win)
+		{
+			// Glass extend WINAPI thingie http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx form more details
+			// If any of the margins is "-1" the whole window is glass!
+			win.GlassBackground(-1, 0, 0, 0);
+		}
+		/// <summary>
+		/// Sets glass background to custom margins in the window.
+		/// </summary>
+		/// <param name="win"></param>
+		public static void GlassBackground(this Window win, int left, int right, int top, int bottom)
+		{
+			// Why would you read the inner workings? Why? If you need to know why...
+			// DwmExtendFrameIntoClientArea http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx is the magical WINAPI call
+			// rest is just crap to get its parameters populated.
+			win.Loaded += delegate(object sender, RoutedEventArgs e)
+			{
+				try
+				{
+					// Obtain the window handle for WPF application
+					IntPtr mainWindowPtr = new WindowInteropHelper(win).Handle;
+					HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+
+					// Transparent shall be glassed!
+					mainWindowSrc.CompositionTarget.BackgroundColor = System.Windows.Media.Colors.Transparent;
+
+					// Margin for the DwmExtendFrameIntoClientArea WINAPI call.
+					NonClientRegionAPI.MARGINS margins = new NonClientRegionAPI.MARGINS();
+					margins.cxLeftWidth = left;
+					margins.cxRightWidth = right;
+					margins.cyBottomHeight = bottom;
+					margins.cyTopHeight = top;
+
+					// Glass extend WINAPI thingie http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx form more details
+					int hr = NonClientRegionAPI.DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+					if (hr < 0)
+					{
+						//DwmExtendFrameIntoClientArea Failed
+					}
+					else
+					{
+						win.Background = System.Windows.Media.Brushes.Transparent;
+					}
+				}
+				// If not glassing capabilities (Windows XP...), paint background white.
+				catch (DllNotFoundException)
+				{
+					System.Windows.Application.Current.MainWindow.Background = System.Windows.Media.Brushes.White;
+				}
+			};
+		}
+
+		private class NonClientRegionAPI
+		{
+			[StructLayout(LayoutKind.Sequential)]
+			public struct MARGINS
+			{
+				public int cxLeftWidth;      // width of left border that retains its size
+				public int cxRightWidth;     // width of right border that retains its size
+				public int cyTopHeight;      // height of top border that retains its size
+				public int cyBottomHeight;   // height of bottom border that retains its size
+			};
+
+
+			[DllImport("DwmApi.dll")]
+			public static extern int DwmExtendFrameIntoClientArea(
+				IntPtr hwnd,
+				ref MARGINS pMarInset);
+
+		}
+	}
+
+}
--- TalesOfVesperiaUtils/TalesOfVesperiaFrontendWPF/MainWindow.xaml.cs
+++ TalesOfVesperiaUtils/TalesOfVesperiaFrontendWPF/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -13,6 +12,8 @@ using TalesOfVesperiaTranslationEngine;
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace TalesOfVesperiaFrontendWPF
 {
@@ -24,7 +25,7 @@ namespace TalesOfVesperiaFrontendWPF
         public MainWindow()
         {
             InitializeComponent();
-            this.GlassBackground(0, 0, 0, 39);
+			this.GlassBackground(0, 0, 0, 39);
 
 			var BuildVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 			this.Title = String.Format("Tales of Vesperia en Español - v{0}", BuildVersion);
@@ -60,75 +61,11 @@ namespace TalesOfVesperiaFrontendWPF
 			return dt;
 		}
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+		private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ODlg = new OpenFileDialog();
-            ODlg.Filter = "Archivos ISO (*.iso)|*.iso|Todos los archivos (*.*)|*.*";
-            ODlg.Title = "Elige la ISO de la versión PAL del Tales of Vesperia";
-            if (ODlg.ShowDialog() == false) return;
-
-            //Make a copy of the ISO
-            string OriginalISOPath = ODlg.FileName;
-            string TranslatedISOPath = Path.Combine(Path.GetDirectoryName(OriginalISOPath), "Tales of Vesperia [PAL] [Español].iso");
-
-			if (OriginalISOPath == TranslatedISOPath)
-			{
-				MessageBox.Show("No se puede actualizar el mismo archivo directamente.", "Hubo un error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
-			}
-
-			var RunTask = Task.Run(() =>
-			{
-				try
-				{
-					this.Dispatcher.Invoke(() =>
-					{
-						this.Parchear.IsEnabled = false;
-					});
-
-					//throw (new Exception("Test ERROR!"));
-
-					UpdateProgress(0, 100, TaskbarItemProgressState.Normal);
-					Console.WriteLine("Copying ISO...");
-					if (!File.Exists(TranslatedISOPath)) File.Copy(OriginalISOPath, TranslatedISOPath);
-					Console.WriteLine("Done");
-
-					UpdateProgress(50, 100, TaskbarItemProgressState.Normal);
-
-					var Patcher = new Patcher(TranslatedISOPath);
-					Patcher.Progress += (Current, Total) =>
-					{
-						UpdateProgress(Current, Total, TaskbarItemProgressState.Normal);
-					};
-					new PatchAll(Patcher).Handle();
-					//Load the ISO
-					/*
-					FileStream ToVISO = new FileStream(TranslatedISOPath, FileMode.Open, FileAccess.ReadWrite);
-					var Dvd9Xbox360 = new Dvd9Xbox360().Load(ToVISO);
-					var Vfs = new Dvd9Xbox360FileSystem(Dvd9Xbox360);
-
-					//Trasteando cosas LOLOLOL
-					ExtractFile(Vfs, "/snd/config.bin", Path.GetDirectoryName(OriginalISOPath));
-            
-					UpdateProgress(75, TaskbarItemProgressState.Normal);
-					*/
-
-					MessageBox.Show("¡Ya se ha traducido Tales of Vesperia al español!", "¡Felicidades!", MessageBoxButton.OK, MessageBoxImage.Information);
-				}
-				catch (Exception Exception)
-				{
-					MessageBox.Show(Exception.ToString(), "Hubo un error. Se ha borrado System32 sin querer.", MessageBoxButton.OK, MessageBoxImage.Error);
-				}
-				finally
-				{
-					this.Dispatcher.Invoke(() =>
-					{
-						this.Parchear.IsEnabled = true;
-					});
-				}
-			});
-
-			//RunTask.Start();
+			Parchear.ContextMenu.PlacementTarget = Parchear;
+			Parchear.ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+			Parchear.ContextMenu.IsOpen = true;
         }
 
         /*
@@ -160,88 +97,31 @@ namespace TalesOfVesperiaFrontendWPF
 				this.TaskbarItemInfo = tb;
 			});
         }
+
+		private void ParchearPopup_LostFocus(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("ParchearPopup_LostFocus");
+		}
+
+		private void PatchIso_Click_1(object sender, RoutedEventArgs e)
+		{
+			var Dialog = new OpenFileDialog();
+			Dialog.Filter = "Archivos ISO (*.iso)|*.iso|Todos los archivos (*.*)|*.*";
+			Dialog.Title = "Elige la ISO de la versión PAL del Tales of Vesperia";
+			if (Dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				PatchThread.Run(Dialog.FileName);
+			}
+		}
+
+		private void PatchFolder_Click_1(object sender, RoutedEventArgs e)
+		{
+			var Dialog = new FolderBrowserDialog();
+			Dialog.Description = "Elige la carpeta de Tales of Vesperia extraída (para JTAG)";
+			if (Dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				PatchThread.Run(Dialog.SelectedPath);
+			}
+		}
     }
-
-    #region Efecto Glass
-
-    public static class GlassingExtension
-    {
-        /// <summary>
-        /// Sets glass background to whole window.
-        /// </summary>
-        /// <remarks>Remember to set your WPF Window Background to "Transparent"!</remarks>
-        /// <param name="win"></param>
-        public static void GlassBackground(this Window win)
-        {
-            // Glass extend WINAPI thingie http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx form more details
-            // If any of the margins is "-1" the whole window is glass!
-            win.GlassBackground(-1, 0, 0, 0);
-        }
-        /// <summary>
-        /// Sets glass background to custom margins in the window.
-        /// </summary>
-        /// <param name="win"></param>
-        public static void GlassBackground(this Window win, int left, int right, int top, int bottom)
-        {
-            // Why would you read the inner workings? Why? If you need to know why...
-            // DwmExtendFrameIntoClientArea http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx is the magical WINAPI call
-            // rest is just crap to get its parameters populated.
-            win.Loaded += delegate(object sender, RoutedEventArgs e)
-            {
-                try
-                {
-                    // Obtain the window handle for WPF application
-                    IntPtr mainWindowPtr = new WindowInteropHelper(win).Handle;
-                    HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
-
-                    // Transparent shall be glassed!
-                    mainWindowSrc.CompositionTarget.BackgroundColor = System.Windows.Media.Colors.Transparent;
-
-                    // Margin for the DwmExtendFrameIntoClientArea WINAPI call.
-                    NonClientRegionAPI.MARGINS margins = new NonClientRegionAPI.MARGINS();
-                    margins.cxLeftWidth = left;
-                    margins.cxRightWidth = right;
-                    margins.cyBottomHeight = bottom;
-                    margins.cyTopHeight = top;
-
-                    // Glass extend WINAPI thingie http://msdn.microsoft.com/en-us/library/aa969512%28VS.85%29.aspx form more details
-                    int hr = NonClientRegionAPI.DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
-                    if (hr < 0)
-                    {
-                        //DwmExtendFrameIntoClientArea Failed
-                    }
-                    else
-                    {
-                        win.Background = System.Windows.Media.Brushes.Transparent;
-                    }
-                }
-                // If not glassing capabilities (Windows XP...), paint background white.
-                catch (DllNotFoundException)
-                {
-                    Application.Current.MainWindow.Background = System.Windows.Media.Brushes.White;
-                }
-            };
-        }
-
-        private class NonClientRegionAPI
-        {
-            [StructLayout(LayoutKind.Sequential)]
-            public struct MARGINS
-            {
-                public int cxLeftWidth;      // width of left border that retains its size
-                public int cxRightWidth;     // width of right border that retains its size
-                public int cyTopHeight;      // height of top border that retains its size
-                public int cyBottomHeight;   // height of bottom border that retains its size
-            };
-
-
-            [DllImport("DwmApi.dll")]
-            public static extern int DwmExtendFrameIntoClientArea(
-                IntPtr hwnd,
-                ref MARGINS pMarInset);
-
-        }
-    }
-
-#endregion
 }
