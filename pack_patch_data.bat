6128f002030ddf4adc4c0a01110df917c063a455
soywiz <soywiz@gmail.com>
2012-11-06 10:18:45 +0100

    - Updates

--- pack_patch_data.bat
+++ pack_patch_data.bat
@@ -1,3 +1,3 @@
 @echo off
-"%~dp0\tools\7z" a -tzip -mx=0 PatchData.zip -r -x!.git -x!obj -x!project/obj -xr!*.psd -xr!*.iros -x!tools -x!deploy.bat -x!.DS_Store PatchData
-"%~dp0\tools\lzma" e PatchData.zip PatchData.zip.lzma -d25 -mt4
+"%~dp0\tools\7z" a -tzip -mx=9 PatchData.zip -r -x!.git -x!obj -x!project/obj -xr!*.psd -xr!*.iros -x!tools -x!deploy.bat -x!.DS_Store PatchData
+REM "%~dp0\tools\lzma" e PatchData.zip PatchData.zip.lzma -d25 -mt4
