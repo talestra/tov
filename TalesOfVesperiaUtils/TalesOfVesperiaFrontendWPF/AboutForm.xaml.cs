9cf36286449dddef1a06bec8c05ddec94296d52d
PacoChan <maikelchan88@gmail.com>
2012-11-06 16:56:29 +0100

    Updated frontend.

---
+++ TalesOfVesperiaUtils/TalesOfVesperiaFrontendWPF/AboutForm.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TalesOfVesperiaFrontendWPF
+{
+    /// <summary>
+    /// Interaction logic for AboutForm.xaml
+    /// </summary>
+    public partial class AboutForm : Window
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
