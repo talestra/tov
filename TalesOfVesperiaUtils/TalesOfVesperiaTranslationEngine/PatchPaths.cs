d60e2d374b0c1e9fbbe3d5009892944865b00d8b
PacoChan <maikelchan88@gmail.com>
2012-11-09 23:34:05 +0100

    Translated and moved some images.

--- TalesOfVesperiaUtils/TalesOfVesperiaTranslationEngine/PatchPaths.cs
+++ TalesOfVesperiaUtils/TalesOfVesperiaTranslationEngine/PatchPaths.cs
@@ -11,30 +11,58 @@ namespace TalesOfVesperiaTranslationEngine
 		public static string TextsTovJson = "Text/tov.json";
 		public static string U_USUALBTLFONT00 = "Font/U_USUALBTLFONT00_EU.png";
 		public static string U_USUALBTLFONT01 = "Images/Battle/E_A034_BTLFONT01_E.png";
+		
+		public static string U_ICONBTN36000 = "Images/Common/U_ICONBTN36000.png";
+		public static string U_USUALFIEINFO02 = "Images/Common/U_USUALFIEINFO02.png";
 		public static string U_USUALLOAD00 = "Images/Common/U_USUALLOAD00.png";
 		public static string U_USUALLOAD01 = "Images/Common/U_USUALLOAD01.png";
-		public static string U_USUALFIEINFO02 = "Images/Common/U_USUALFIEINFO02.png";
 
 		public static string E_A018_LVUP_00 = "Images/Battle/E_A018_LVUP_00.png";
 		public static string E_A019_NEW_00 = "Images/Battle/E_A019_NEW_00.png";
 		public static string E_A024_LEARNING_00 = "Images/Battle/E_A024_LEARNING_00.png";
 		public static string E_A027_MISSION_00 = "Images/Battle/E_A027_MISSION_00.png";
 		public static string E_A062_TITLE_00 = "Images/Battle/E_A062_TITLE_00.png";
+		
+		public static string U_MG_BR = "Images/Minigames/U_MG_BR.png";
+		public static string U_MINIGAMEISHI01E = "Images/Minigames/U_MINIGAMEISHI01E.png";
+		public static string U_MINIGAMEISHI02E = "Images/Minigames/U_MINIGAMEISHI02E.png";
+		public static string U_MINIGAMEPOKER00 = "Images/Minigames/U_MINIGAMEPOKER00.png";
+		public static string U_MINIGAMEPOKER01 = "Images/Minigames/U_MINIGAMEPOKER01.png";
+		public static string U_MINIGAMEPOKER04 = "Images/Minigames/U_MINIGAMEPOKER04.png";
+		public static string U_MINIGAMEPOKER05 = "Images/Minigames/U_MINIGAMEPOKER05.png";
+		public static string U_MINIGAMEPOKER06 = "Images/Minigames/U_MINIGAMEPOKER06.png";
+		public static string U_MINIGAMEPOKER07 = "Images/Minigames/U_MINIGAMEPOKER07.png";
+		public static string U_MINIGAMEPOKER08 = "Images/Minigames/U_MINIGAMEPOKER08.png";
+		public static string U_MINIGAMEPOKER09 = "Images/Minigames/U_MINIGAMEPOKER09.png";
+		public static string U_MINIGAMEPOKER10 = "Images/Minigames/U_MINIGAMEPOKER10.png";
+		public static string U_MINIGAMEPOKER11 = "Images/Minigames/U_MINIGAMEPOKER11.png";
+		public static string U_MINIGAMEPOKER12 = "Images/Minigames/U_MINIGAMEPOKER12.png";
+		public static string U_MINIGAMEPOKER13 = "Images/Minigames/U_MINIGAMEPOKER13.png";
+		public static string U_MINIGAMEPOKER14 = "Images/Minigames/U_MINIGAMEPOKER14.png";
+		public static string U_MINIGAMEPOKER15 = "Images/Minigames/U_MINIGAMEPOKER15.png";
+		public static string U_MINIGAMEPOKER70 = "Images/Minigames/U_MINIGAMEPOKER70.png";
+		public static string U_MINIGAMERACE01 = "Images/Minigames/U_MINIGAMERACE01.png";
+		public static string U_MINIGAMERACE02 = "Images/Minigames/U_MINIGAMERACE02.png";
+		public static string U_MINIGAMERACE03 = "Images/Minigames/U_MINIGAMERACE03.png";
 
 		public static string E_A101_TITLE_P001 = "Images/Title/E_A101_TITLE_P001.png";
 		public static string E_A101_TITLE_PUSH = "Images/Title/E_A101_TITLE_PUSH.png";
 		public static string E_A101_TITLE_CREDIT_E = "Images/Title/E_A101_TITLE_CREDIT_E.png";
+		public static string E_A101_TITLE_TEAM = "Images/Title/E_A101_TITLE_TEAM.png";
 
-		public static string U_COMALLINFO00 = "Images/Menu/COMALL.U_COMALLINFO00.png";
-		public static string U_ICONBTN36000 = "Images/Common/ICONBTN360.U_ICONBTN36000.png";
+		public static string U_COMALLINFO00 = "Images/Menu/U_COMALLINFO00.png";
+		public static string U_ICONKIND00 = "Images/Menu/U_ICONKIND00.png";
+		public static string U_ICONSORT00 = "Images/Menu/U_ICONSORT00.png";
+		public static string U_ICONSORT01 = "Images/Menu/U_ICONSORT01.png";
+		public static string U_ICONSYM01 = "Images/Menu/U_ICONSYM01.png";
+		public static string U_MENUSTR01 = "Images/Menu/U_MENUSTR01.png";
+		
 		public static string U_ICONKIND00 = "Images/ICONKIND.U_ICONKIND00.png";
-		public static string U_ICONSORT00 = "Images/Menu/ICONSORT.U_ICONSORT00.png";
-		public static string U_ICONSORT01 = "Images/Menu/ICONSORT.U_ICONSORT01.png";
 		public static string U_ICONSYM01 = "Images/ICONSYM.U_ICONSYM01.png";
 		public static string U_MENUBTLINFO00 = "Images/Battle/MENU.U_MENUBTLINFO00.png";
 		public static string U_MENUBTLINFO02 = "Images/Battle/MENU.U_MENUBTLINFO02.png";
 		public static string U_MENUSTR01 = "Images/MENU.U_MENUSTR01.png";
-
-        public static string FONTTEXT10_15 = "Font/FONTTEX10.FONTTEX10.15.png";
-    }
+		
+		public static string FONTTEXT10_15 = "Font/FONTTEX10.FONTTEX10.15.png";
+	}
 }
